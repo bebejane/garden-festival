a78273f3c60e9ec937ea355459031137fa8f327f
Bebe Jane <bjornthief@gmail.com>
2021-11-29 20:49:25 +0100

    responsive videos

--- components/blocks/Video.js
+++ components/blocks/Video.js
@@ -1,12 +1,15 @@
 import styles from "./Video.module.scss"
 import cn from "classnames";
 import { useEffect, useRef, useState } from "react";
+import {useWindowSize} from "rooks"
 
 export default function Video({provider, providerUid, title, url, thumbnailUrl}) {
 	const ref = useRef()
 	const [height, setHeight] = useState(360);
-	useEffect(()=>setHeight((ref.current?.clientWidth/16)*9), []) // Set to 16:9
-
+	const {innerWidth} = useWindowSize()
+	
+	useEffect(()=>setHeight((ref.current?.clientWidth/16)*9), [innerWidth]) // Set to 16:9
+	
 	return (
 		provider === 'youtube' ?
 			<iframe 
