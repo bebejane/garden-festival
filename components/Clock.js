a78557c90e0d329f19492c53496b96654af17fd1
Bebe Jane <bjornthief@gmail.com>
2021-12-03 14:14:18 +0100

    fix init clock

--- components/Clock.js
+++ components/Clock.js
@@ -4,19 +4,24 @@ import React, { useState, useEffect, useRef, useReducer } from "react";
 import Link from "next/link"
 import { useForkRef, useInterval } from "rooks";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    default:
-      return state;
+const clockSize = 90;
+
+const generateStyles = (style) => {
+  const d = new Date()
+  const h = (d.getHours()/12)*360
+  const m = (d.getMinutes()/60)*360
+  const s = (d.getSeconds()/60)*360
+
+  return {
+    hour: {...style.hour, transform: `rotate(${h}deg) translateY(${(clockSize - style.hour.height)/2}px) translateX(${clockSize/2}px)`},
+    minute: {...style.minute, transform: `rotate(${m}deg) translateY(${(clockSize - style.minute.height)/2}px) translateX(${clockSize/2}px)`},
+    second: {...style.second, transform: `rotate(${s}deg) translateY(${(clockSize - style.second.height)/2}px) translateX(${clockSize/2}px)`},
   }
 }
-
 export default function Clock() {
   
-  const clockSize = 90;
-  const [ style, setStyle ] = useState({
+  
+  const [ style, setStyle ] = useState(generateStyles({
     hour:{
       height:4
     },
@@ -26,21 +31,12 @@ export default function Clock() {
     second:{
       height:1
     }
-  })
-  const { start, stop } = useInterval(() => updateTime(), 1000);
+  }))
+  const { start, stop } = useInterval(() => setStyle(generateStyles(style)), 1000);
 
+  
   const updateTime = () => { 
     
-    const d = new Date()
-    const h = (d.getHours()/12)*360
-    const m = (d.getMinutes()/60)*360
-    const s = (d.getSeconds()/60)*360
-
-    setStyle({
-      hour: {...style.hour, transform: `rotate(${h}deg) translateY(${(clockSize - style.hour.height)/2}px) translateX(${clockSize/2}px)`},
-      minute: {...style.minute, transform: `rotate(${m}deg) translateY(${(clockSize - style.minute.height)/2}px) translateX(${clockSize/2}px)`},
-      second: {...style.second, transform: `rotate(${s}deg) translateY(${(clockSize - style.second.height)/2}px) translateX(${clockSize/2}px)`},
-    })
   }
 
   useEffect(()=> start(), [])
