3498d2f4c4e7b4e2334b39fd10d9efaf91491c0c
konst-teknik <information-konst-teknik.se>
2021-12-13 11:00:03 +0100

    navtypo

--- components/views/Festival.js
+++ components/views/Festival.js
@@ -8,45 +8,45 @@ import { formatToTimeZone } from 'date-fns-timezone'
 import { useEffect } from "react";
 import Link from "next/link"
 
-export default function Festival({events, dayEvents, participants, date, timeZone, show}) {
-  if(!show) return null
+export default function Festival({ events, dayEvents, participants, date, timeZone, show }) {
+  if (!show) return null
 
   const [appState, setAppState] = useAppState();
-  useEffect(()=>{
+  useEffect(() => {
     return
     count++;
-    if(count === 1) return
+    if (count === 1) return
     const el = document.getElementById(`${format(appState.date, 'yyyy-MM-d')}`)
-    el?.scrollIntoView({ block:"center", behavior: "smooth"})
+    el?.scrollIntoView({ block: "center", behavior: "smooth" })
     console.log('scroll')
-    
+
   }, [appState.date])
 
-  
-  
+
+
   let currentDate;
-  const schedule = [...(dayEvents || events)].sort((a,b) => new Date(a.startTime) > new Date(b.startTime)).map((ev, idx) => {    
+  const schedule = [...(dayEvents || events)].sort((a, b) => new Date(a.startTime) > new Date(b.startTime)).map((ev, idx) => {
     let eventDate;
-    if(!isSameDay(currentDate, new Date(ev.startTime))){
+    if (!isSameDay(currentDate, new Date(ev.startTime))) {
       currentDate = new Date(ev.startTime);
       eventDate = currentDate;
     }
     return (
       <>
-        {eventDate && 
+        {eventDate &&
           <h1 id={format(eventDate, 'yyyy-MM-d')} className={styles.weekday}>
             {format(eventDate, 'EEEE MMMM d, yyyy')}
           </h1>
         }
-        <div key={idx} className={styles.event}>
+        <a key={idx} className={styles.event} href={`/${ev.participant.slug}/${ev.slug}`}>
           <div className={styles.symbol}>
             <Link href={`/${ev.participant.slug}/${ev.slug}`}>
               <a>
-                <img 
+                <img
                   id={`festival-symbol-${ev.id}`}
                   eventid={ev.id}
                   participantid={ev.participant.id}
-                  src={`${ev.participant.symbol.url}?w=200`} 
+                  src={`${ev.participant.symbol.url}?w=200`}
                   className={contentStyles.placeholderSymbol}
                 />
               </a>
@@ -54,28 +54,28 @@ export default function Festival({events, dayEvents, participants, date, timeZon
           </div>
           <div className={styles.info}>
             <p>
-              {ev.startTime && 
-                <span>
-                  {formatToTimeZone(ev.startTime, 'dddd MMMM D, HH:mm z', { timeZone: appState.zone.timeZone })}
+              {ev.startTime &&
+                <span class="meta">
+                  {formatToTimeZone(ev.startTime, 'HH:mm z', { timeZone: appState.zone.timeZone })}
                 </span>
               }
-              <h3>{ev.title}</h3>
-              <br/>
+              <h2>{ev.title}</h2>
+              <br />
               {ev.summary}
             </p>
             <p>
-            <LinkButton href={`/${ev.participant.slug}/${ev.slug}`}>  
-              Go to event
-            </LinkButton>
+              <LinkButton href={`/${ev.participant.slug}/${ev.slug}`}>
+                Go to event
+              </LinkButton>
             </p>
           </div>
-        </div>
+        </a>
       </>
     )
   })
-	return (
-		<div className={styles.festival}>
+  return (
+    <div className={styles.festival}>
       {schedule}
-		</div>
-	);
+    </div>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -9,6 +9,8 @@ const sassOptions = {
     @import "./styles/partials/styles";
     @import "./styles/partials/variables";
     @import "./styles/partials/fonts";
+    @import "./styles/partials/typography";
+
   `
 }
 const nextOptions = {
