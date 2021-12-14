d93457f8eb2e723a4399986e9f3769a6d6396a6c
Bebe Jane <bjornthief@gmail.com>
2021-12-14 12:52:58 +0100

    fix releaseTime graphql query

--- components/Symbol.js
+++ components/Symbol.js
@@ -8,47 +8,10 @@ import { useRouter } from "next/router";
 import { useHover } from "/lib/hooks";
 
 const Symbol = (props) => {
-	
 	const { event, symbolSize, index } = props;
-	const router = useRouter();
-	const [disabled, setDisabled] = useState(false);
-	const [to, setTo] = useState();
 	const [ref, hovering] = useHover();
 	
-	const togglePopup = (on) => {
-		clearTimeout(to);
-		if (disabled) return;
-		
-		let timeout = setTimeout(
-			() => {
-				const eventId = ref.current.getAttribute("eventid");
-				const popup = document.getElementById(`garden-popup-${eventId}`);
-				const { offsetTop: top, offsetLeft: left, clientWidth: width, clientHeight:height } = ref.current;
-				const { clientWidth: windowWidth } = document.body;
-				const t = top - (symbolSize / 1.5)
-				const l = (left + width) - (symbolSize / 3);
-
-				popup.style.top = `${Math.max(t, 0)}px`;
-				popup.style.left = `${Math.min(l, windowWidth-width)}px`;
-				popup.classList.toggle(styles.show, on);
-			},
-			on ? 100 : 0
-		);
-		setTo(timeout)
-	};
-
-	const disablePopup = () => {
-		const popup = document.getElementById(`garden-popup-${event.id}`);
-		popup.classList.remove(styles.show);
-		setDisabled(true);
-		setTimeout(() => setDisabled(false), 2000);
-	};
-	useEffect(() => {
-		router.events.on("routeChangeStart", disablePopup);
-		return () => router.events.off("routeChangeStart", disablePopup);
-	}, []);
-
-	useEffect(() => togglePopup(hovering), [hovering]);
+	//useEffect(() => togglePopup(hovering), [hovering]);
 
 	return (
 		<>
@@ -65,11 +28,7 @@ const Symbol = (props) => {
 					/>
 				</a>
 			</Link>
-			<div id={`garden-popup-${event.id}`} className={styles.symbolPopup}>
-				<h3>{event.title}</h3>
-				<p>{event.summary.split(".")[0]}</p>
-				{format(new Date(event.startTime), "EEEE MMMM d, yyyy ")}
-			</div>
+			<PopUp event={event} show={hovering} symbolSize={symbolSize}/>
 			<img
 				id={`symbol-${event.id}`}
 				key={`symbol-${index}`}
@@ -82,5 +41,55 @@ const Symbol = (props) => {
 		</>
 	);
 };
+
+
+const PopUp = ({event, symbolSize, show}) =>{
+
+	const [to, setTo] = useState();
+	const [disabled, setDisabled] = useState(false);
+	const router = useRouter();
+
+	const disablePopup = () => {
+		const popup = document.getElementById(`garden-popup-${event.id}`);
+		popup.classList.remove(styles.show);
+		setDisabled(true);
+		setTimeout(() => setDisabled(false), 2000);
+	};
+
+	const togglePopup = (on) => {
+		clearTimeout(to);
+		if (disabled) return;
+		
+		let timeout = setTimeout(() => {
+				const pad = 20;
+				const el = document.getElementById(`garden-symbol-${event.id}`);
+				const popup = document.getElementById(`garden-popup-${event.id}`);
+				const { offsetTop: top, offsetLeft: left, clientWidth: width, clientHeight:height } = el;
+				const { clientWidth : popupWidth, clientHeight : popupHeight } = popup
+				const { clientWidth: windowWidth } = document.body;
+				const t = top + symbolSize + pad;
+				const l = left//(left) - ((popupWidth - width)/2);
+				//console.log(((popupWidth - width)/2), popupWidth, width, popup)
+
+				popup.style.top = `${Math.max(t, 0)}px`;
+				popup.style.left = `${Math.min(l, windowWidth-width)}px`;
+				popup.classList.toggle(styles.show, on);
+		}, on ? 100 : 0);
+		setTo(timeout)
+	};
+	useEffect(()=>togglePopup(show), [show])
+	useEffect(() => {
+		router.events.on("routeChangeStart", disablePopup);
+		return () => router.events.off("routeChangeStart", disablePopup);
+	}, []);
+
+	return (
+		<div id={`garden-popup-${event.id}`} className={styles.symbolPopup}>
+			<h3>{event.title}</h3>
+			<p>{event.summary.split(".")[0]}</p>
+			{format(new Date(event.startTime), "EEEE MMMM d, yyyy ")}
+		</div>
+	)
+}
 
 export default Symbol;
