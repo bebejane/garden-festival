d0dadb3ab12230810a7de46b93c694a6409d7b3e
konst-teknik <information-konst-teknik.se>
2022-01-04 14:10:12 +0100

    Color

--- components/content/ContentMain.js
+++ components/content/ContentMain.js
@@ -1,9 +1,9 @@
 import styles from "./ContentMain.module.scss";
 import cn from "classnames";
 
-export default function ContentHeader({ children, black, color }) {
+export default function ContentMain({ children, black, color }) {
 	return (
-		<div className={cn(styles.contentMain, black && styles.black, color && color)}>
+		<div className={cn(styles.contentMain, black && styles.black, color && styles[color])}>
 			{children}
 		</div>
 	);
