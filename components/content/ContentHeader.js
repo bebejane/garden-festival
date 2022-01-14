143059d500c0e15b04dbb643930445834cd65c34
konst-teknik <information-konst-teknik.se>
2022-01-14 13:17:32 +0100

    Background on Participant

--- components/content/ContentHeader.js
+++ components/content/ContentHeader.js
@@ -4,7 +4,7 @@ import { Image as DatoImage } from 'react-datocms'
 
 export default function ContentHeader({ children, responsiveImage, black, color, type }) {
 	return (
-		<div className={cn(styles.contentHeader, !responsiveImage && styles.bgColor, black && styles.black, color && styles[color], type && styles[type])}>
+		<div className={cn(styles.contentHeader, !responsiveImage && styles.bgColor, black && styles.black, !responsiveImage && styles[color], type && styles[type])}>
 			{responsiveImage && <DatoImage className={styles.bgImage} data={responsiveImage} />}
 			{children}
 		</div>
