e66ef93bfade9f962f5d10af0b0647fe198cb744
konst-teknik <information-konst-teknik.se>
2022-01-14 11:09:38 +0100

    Participants

--- components/views/Event.js
+++ components/views/Event.js
@@ -19,7 +19,7 @@ export default function Event({ event, events, show, symbolSize }) {
       <div className={styles.info}>
         <ContentHeader responsiveImage={event.image?.responsiveImage} color={event.participant.color}>
           <header>
-            <section className="meta"><span className="meta">{format(new Date(event.startTime), 'EEEE MMMM d ')}</span></section>
+            <section className="meta"><span className="meta">{format(new Date(event.startTime), 'EEE MMM d H:mm')}</span></section>
             <figure>
               <img
                 id={`event-symbol-${event?.id}`}
