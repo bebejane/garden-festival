1f99b4328f2f0e7bf5e30e8f4dafe6cb3580ac5e
Bebe Jane <bjornthief@gmail.com>
2022-01-21 17:01:48 +0100

    fix

--- components/views/Festival.js
+++ components/views/Festival.js
@@ -24,7 +24,7 @@ export default function Festival({ events, dayEvents, participants, date, timeZo
     const durationUntil = intervalToDuration({start: new Date(), end: new Date(ev.startTime)})
     
     const eventContent = (
-      <a className={cn(styles.event, !ev.register && ev.launched && styles.launched)}>
+      <a className={cn(styles.event, ev.launched && styles.launched)}>
         <div className={styles.symbol}>
           <img
             id={`${view}-symbol-${ev.id}`}
