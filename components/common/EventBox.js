40cf6aea1d21d75525f73e218da4a31fa4b1c0c8
Bebe Jane <bjornthief@gmail.com>
2022-01-25 11:49:40 -0500

    add more timezones

--- components/common/EventBox.js
+++ components/common/EventBox.js
@@ -55,7 +55,7 @@ const EventBox = ({ event, view, symbolSize }) => {
         <p>
           {event.startTime &&
             <span className="meta">
-              {formatInTimeZone(new Date(event.startTime), appState.zone.timeZone, 'EEE HH:mm')} • {event.typeOfEvent?.title} • By {event.participant.title}
+              {event.isPreParty ? 'Pre Party' : event.isAfterParty ? 'After party' : formatInTimeZone(new Date(event.startTime), appState.zone.timeZone, 'EEE HH:mm')} • {event.typeOfEvent?.title} • By {event.participant.title}
             </span>
           }
           <h2>{event.title}</h2>
